aa12e2c9dc191a8fd1c9a5a17874b712a308e7f5
Muhammad Zharif Fadzli Rusli <zhariffadzli@gmail.com>
2018-10-22 16:08:39 +0300

    SimpleClass + Constructor

--- src/SimpleClass.kt
+++ src/SimpleClass.kt
@@ -2,3 +2,55 @@
  * Created by Muhammad Zharif Fadzli Bin Rusli zhariffadzli@gmail.com
  */
 
+
+
+class Car(){
+    var type:String?=null
+    var model:Int?=null
+    var price:Double?=null
+    var milesDrive:Int?= null
+    var owner:String?=null
+    constructor( type:String, model:Int, price:Double,
+                  milesDrive:Int, owner:String):this() {
+        this.type = type
+        this.model = model
+        this.price = price
+        this.milesDrive = milesDrive
+        this.owner = owner
+        println("Constructor 1")
+    }
+
+
+    constructor(owner:String):this(){
+        this.owner=owner
+        println("constructor2")
+
+    }
+
+//    init{
+//        println("Details of the Car is Type")
+//        println("Type: $type")
+//        println("Year Manufactured: $model")
+//        println("Price: $price")
+//        println("Miles Drive: $milesDrive")
+//        println("Owner: $owner")
+//    }
+
+    fun getCarPrice():Double{
+        return this.price!! - (this.milesDrive!!.toDouble()*10)
+
+    }
+
+}
+
+
+fun main(args:Array<String>){
+
+    val zhCar=Car("BMW",2015,10000.10,105, "Zharif")
+    println("Zharif Car: " +zhCar.getCarPrice())
+    val AmyCar=Car("Mercedes", 2016,5000.0, 205, "Wan Amira")
+    println("Amy Car: " +AmyCar.getCarPrice())
+
+
+    val laCar=Car("Zharif")
+}
